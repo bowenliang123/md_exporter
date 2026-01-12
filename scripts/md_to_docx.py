2b398cf5d1c4d626015b1449bb2896e716401f94
bowenliang123 <liang.bowen.123@qq.com>
2026-01-12 22:27:39 +0800

    move template files

--- scripts/md_to_docx.py
+++ scripts/md_to_docx.py
@@ -8,6 +8,9 @@ import argparse
 import sys
 from pathlib import Path
 
+# Add the scripts directory to Python path to fix import issues
+sys.path.insert(0, str(Path(__file__).resolve().parent))
+
 # Import shared utility functions
 from lib.svc_md_to_docx import convert_md_to_docx, get_default_template
 
