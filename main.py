1a0b2e580444cfb483ee8d0e39219f8c01fbba54
bowenliang123 <liang.bowen.123@qq.com>
2026-01-19 02:46:56 +0800

    add Dify plugin logger handler when loading as Dify plugin

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import os
+
 from dify_plugin import DifyPluginEnv, Plugin
+
+# set system LOAD_FROM_DIFY_PLUGIN to 1 as mark of Dify entry
+os.environ['LOAD_FROM_DIFY_PLUGIN'] = "1"
 
 plugin = Plugin(DifyPluginEnv())
 
